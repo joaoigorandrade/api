ca60fc3888fdd1e4f783955f06f27cfb75586511
joao igor <joaoigora@gmail.com>
2021-06-13 12:25:50 -0300

    tres

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../database/posts");
+const paises = require("../database/posts");
 const copa = require("../database/users");
 
-router.get("/post", (req, res, next) => {
-  const filter = req.query.number || 100;
-  const postResponse = posts.filter((post, index) => index < filter);
+router.get("/paises", (req, res, next) => {
   res.status(200).json({
     message: "OK",
-    posts: postResponse,
+    posts: paises,
   });
 });
 router.get("/copa", (req, res, next) => {
-  // if (req.query.id <= 0 || req.query.id > 11)
-  //   return res.json({
-  //     message: "Something goes wrong",
-  //   });
   res.json({
     message: "OK",
     user: copa,
