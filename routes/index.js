b76d29095e1382af15cf043a4cbe563ee93e0904
joaoigor <joaoigora@gmail.com>
2021-06-14 20:30:24 -0300

    finish

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const paises = require("../database/posts");
-const copa = require("../database/users");
+const paises = require("../database/paises");
+const copa = require("../database/copa");
 
 router.get("/paises", (req, res, next) => {
-  res.status(200).json({
-    message: "OK",
-    posts: paises,
-  });
+  res.status(200).json(
+    paises
+  );
 });
 router.get("/copa", (req, res, next) => {
-  res.json({
-    message: "OK",
-    user: copa,
-  });
+  res.json(copa,
+  );
 });
 
 module.exports = router;
